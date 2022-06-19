ff6e890d8a5fb0aa5457d057013faede28e74658
JulianaCarvajal <julianacarvajalguerra@gmail.com>
2022-06-19 08:54:52 -0500

    Update

--- punto_1.py
+++ punto_1.py
@@ -5,11 +5,12 @@ Ten presente que los datos con * son los más importantes, si uno de esos campos
 en caso de que el dato solicitado no sea importante, el programa deberá poner “unknown”.
 """
 
-def family_members(line):
+def family_members(line, writer):
     """Check if the line is a family member's line
 
     Args:
         line (str): current line in the file
+        writer (file): file to write the information
 
     Returns:
         bool: sentinel
@@ -52,24 +53,28 @@ def fill_form(line):
             entry = 'unknown'
     return line.rstrip('\n') + ' ' + entry + '\n'
 
-# Open and close the file:
-with open('required_data.txt', 'r') as reader, open('filled_form.txt', 'w') as writer:
+def main():
+    """Main function"""
+    # Open and close the file:
+    with open('required_data.txt', 'r') as reader, open('filled_form.txt', 'w') as writer:
 
-    print('Enter your personal data \n')
-    # Iterating over each line in the file:
-    for line in reader:
-        # Select the lines on which information is to be filled in:
-        if ':' in line:
-            # Check if the line is a family member:
-            sent = family_members(line)
-            # If there's a family member in the line, skip it:
-            if sent == True:
-                continue
+        print('Enter your personal data \n')
+        # Iterating over each line in the file:
+        for line in reader:
+            # Select the lines on which information is to be filled in:
+            if ':' in line:
+                # Check if the line is a family member:
+                sent = family_members(line, writer)
+                # If there's a family member in the line, skip it:
+                if sent == True:
+                    continue
+                else:
+                    # If there's no family member in the line, ask for the information:
+                    new_line = fill_form(line)
+            # Write the same line if it is not a line to be filled in:               
             else:
-                # If there's no family member in the line, ask for the information:
-                new_line = fill_form(line)
-        # Write the same line if it is not a line to be filled in:               
-        else:
-            new_line = line
-        # Write the information in the file:    
-        writer.write(new_line)
+                new_line = line
+            # Write the information in the file:    
+            writer.write(new_line)
+
+main()
